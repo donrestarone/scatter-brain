722ee19ba6004c7eee965704aeb76d486040189c
donrestarone <shashikejayatunge@gmail.com>
2019-09-08 14:08:10 -0400

    add redux pub sub for dispatching the add new fact event and initialize timer

--- scatter-brain/src/App.js
+++ scatter-brain/src/App.js
@@ -1,14 +1,83 @@
-import React from 'react';
+
 import {getRandomFact} from './Services/getRandomFact'
 import './App.css';
+import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import Slider from './Components/Slider/Slider'
+import FactHOC from './Components/HOC/FactHOC/FactHOC'
+class App extends Component {
+  
+  componentWillMount = () => {
+    this.initializeDataFeed()
+    this.listenForAppClose()
+  }
 
-function App() {
-  console.log(getRandomFact().then(response => response.json()).then(object => console.log(object)))
-  return (
-    <div className="App">
+  componentWillReceiveProps = (newProps) => {
+    this.checkForSpeedChange(newProps)
+  }
 
-    </div>
-  );
+  checkForSpeedChange = (newProps) => {
+    if (newProps.speedValue !== this.props.speedValue) {
+      this.killTimer()
+      this.initializeDataFeed(this.translateSpeedValue(newProps.speedValue))
+    }
+  }
+
+  translateSpeedValue = (speed) => {
+    return ((100 - speed) + 1) * 40
+  }
+
+  listenForAppClose = (e) => {
+    window.addEventListener('beforeunload', () => {
+      // to prevent memory leaks
+      e.preventDefault()
+      this.killTimer()
+    })
+  }
+
+  killTimer = () => {
+    let timerId = this.props.timer
+    clearInterval(timerId)
+  }
+
+  initializeDataFeed = (interval=2000) => {
+    let timer = setInterval(() => {
+      getRandomFact().then(response => response.json())
+      .then(object => this.props.addNewFact(object))
+    }, interval);
+    this.props.initializeTimer(timer)
+  }
+
+
+
+  render() {
+    return (
+      <>
+        <Slider></Slider>
+        <FactHOC></FactHOC>
+      </>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    addNewFact: (data) => dispatch({
+      type: 'addFact',
+      data
+    }),
+    initializeTimer: (timer) => dispatch({
+      type: 'initTimer',
+      data: timer
+    }),
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    timer: state.timerId,
+    speedValue: state.speed
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
