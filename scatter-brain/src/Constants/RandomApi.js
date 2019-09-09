1275d938ffe4556ff8ead7bfbf9c95a04167a7b0
donrestarone <shashikejayatunge@gmail.com>
2019-09-08 20:17:07 -0400

    add some documentation and remove yarn.lock

--- scatter-brain/src/Constants/RandomApi.js
+++ scatter-brain/src/Constants/RandomApi.js
@@ -1,1 +1,2 @@
-export const randomApiRoot = 'https://uselessfacts.jsph.pl/random.json?language=en'
+// use cors anywhere as a proxy which circumvents the browser preflight check
+export const randomApiRoot = `https://cors-anywhere.herokuapp.com/https://uselessfacts.jsph.pl/random.json?language=en`
